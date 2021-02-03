df8b18c3e743c0dc66dbd211eec17dd0b1802c10
sebas0ne <sebastian_017@hotmail.es>
2021-02-02 23:34:12 -0500

    Responsive home

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -22,6 +22,15 @@
           <span>Click para ingresar a la app.</span>
         </v-tooltip>
       </v-container>
+      <v-row>
+        <v-img
+          alt="Gif de la evolución de Sharizar"
+          id="landing_sharizar"
+          class="shrink mr-2"
+          contain
+          src="@/assets/sharizar.gif"
+        />
+      </v-row>
     </v-container>
     <div class="img_poke_landing"></div>
   </div>
@@ -34,7 +43,8 @@ export default {
   name: "Home",
   data() {
     return {
-      dialog: false
+      dialog: false,
+      alignments: ["start"]
     };
   },
   methods: {
@@ -49,12 +59,63 @@ export default {
 </script>
 
 <style scoped>
-.img_poke_landing {
-  position: absolute;
-  background-image: url("https://steemitimages.com/p/JvFFVmatwWHRfvmtd53nmEJ94xpKydwmbSC5H5svBACH7y78ZHMRmMqDV2AcjNkGNwnpYBAdgqF94io27tUAtTtKjEpW23k45dBCCahQG1kmHDNLZ9aaoiKAV7hgw4wpm8dbgvKQ1k?format=match&mode=fit&width=640");
-  background-repeat: repeat-x;
-  bottom: 60px;
-  height: 150px;
-  width: 100%;
+@media screen and (min-width: 600px) {
+  .img_poke_landing {
+    position: absolute;
+    background-image: url("https://steemitimages.com/p/JvFFVmatwWHRfvmtd53nmEJ94xpKydwmbSC5H5svBACH7y78ZHMRmMqDV2AcjNkGNwnpYBAdgqF94io27tUAtTtKjEpW23k45dBCCahQG1kmHDNLZ9aaoiKAV7hgw4wpm8dbgvKQ1k?format=match&mode=fit&width=640");
+    background-repeat: repeat-x;
+    bottom: 60px;
+    height: 150px;
+    width: 100%;
+  }
+  #landing_sharizar {
+    width: 536px;
+    height: 300;
+  }
+}
+
+@media screen and (max-width: 900px) {
+  .img_poke_landing {
+    position: absolute;
+    background-image: url("https://steemitimages.com/p/JvFFVmatwWHRfvmtd53nmEJ94xpKydwmbSC5H5svBACH7y78ZHMRmMqDV2AcjNkGNwnpYBAdgqF94io27tUAtTtKjEpW23k45dBCCahQG1kmHDNLZ9aaoiKAV7hgw4wpm8dbgvKQ1k?format=match&mode=fit&width=640");
+    background-repeat: repeat-x;
+    bottom: 60px;
+    height: 150px;
+    width: 100%;
+  }
+  #landing_sharizar {
+    width: 526px;
+    height: 280;
+  }
+}
+
+@media screen and (max-width: 1024px) {
+  .img_poke_landing {
+    position: absolute;
+    background-image: url("https://steemitimages.com/p/JvFFVmatwWHRfvmtd53nmEJ94xpKydwmbSC5H5svBACH7y78ZHMRmMqDV2AcjNkGNwnpYBAdgqF94io27tUAtTtKjEpW23k45dBCCahQG1kmHDNLZ9aaoiKAV7hgw4wpm8dbgvKQ1k?format=match&mode=fit&width=640");
+    background-repeat: repeat-x;
+    bottom: 60px;
+    height: 150px;
+    width: 100%;
+  }
+  #landing_sharizar {
+    width: 526px;
+    height: 250;
+  }
+}
+
+@media screen and (max-width: 1380px) {
+  .img_poke_landing {
+    position: absolute;
+    background-image: url("https://steemitimages.com/p/JvFFVmatwWHRfvmtd53nmEJ94xpKydwmbSC5H5svBACH7y78ZHMRmMqDV2AcjNkGNwnpYBAdgqF94io27tUAtTtKjEpW23k45dBCCahQG1kmHDNLZ9aaoiKAV7hgw4wpm8dbgvKQ1k?format=match&mode=fit&width=640");
+    background-repeat: repeat-x;
+    bottom: 60px;
+    height: 150px;
+    width: 100%;
+  }
+  #landing_sharizar {
+    width: 526px;
+    height: 280;
+  }
 }
 </style>
